2802b9752f632362e92990d60b9cb8d56d38ed46
Robert Wensley <robert@brand-science.com>
2019-05-28 01:47:02 -0400

    First commit

---
+++ js/delivery.js
@@ -0,0 +1,11 @@
+var animation = bodymovin.loadAnimation({
+container: document.getElementById('deliveryAnimation'),
+
+// Set your ID to something that you'll associate with the animation you're using //
+renderer: 'svg',
+loop: false,
+autoplay: true,
+path: 'https://assets7.lottiefiles.com/datafiles/uoZvuyyqr04CpMr/data.json'
+
+// Make sure your path has the same filename as your animated     SVG's JSON file //
+})
